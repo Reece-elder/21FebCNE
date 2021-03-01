e7825e81eee9d318eae109cca1b84bb7e4d41ff4
reece-elder <reece.elder@qa.com>
2021-03-01 14:38:55 +0000

    RE: Added JS STuff

---
+++ Frontend/JS/002-Console.log/index.js
@@ -0,0 +1,24 @@
+// * Creates a pop up on the screen
+// alert("hello");
+
+// * Prints to the console
+console.log("Hello World");
+
+// * Four different types of console available
+console.log("log");
+console.info("info");
+console.warn("warning");
+console.error("error");
+
+// ? Creating a variable string called fruit
+let fruit = "banana"; 
+
+// *Input variable directly into the console
+console.log("I have bought a " + fruit + " from the shop!");
+
+// * Allow to use css styling directly in the console
+
+let myText = "wow, what a message";
+
+// * Everything after %c has been styled using CSS Rules formatted at end
+console.log("this is a %c a message with some CSS. " + myText,"color: blue; font-size: larger");
