70aedd3f764a277bf19dcedf774f99b8986544c7
reece-elder <reece.elder@qa.com>
2021-03-03 10:18:47 +0000

    RE: DOM Manipulation

--- Frontend/JS/014-DOMManipulation/index.js
+++ Frontend/JS/014-DOMManipulation/index.js
@@ -91,25 +91,122 @@ jsButton2.addEventListener('click', jsButtonClicked);
 
 // * Create Element + Append Child
 
-let createElement = () => {
-    let newParagraph = document.document.createElement('p');
+// let createElement = () => {
+//     let newParagraph = document.document.createElement('p');
+// }
+
+// Grabbed Create element Button
+let createElementBtn = document.querySelector('.createFunctionBtn');
+
+// paragraph div wrapper saved to paragraphDiv
+let paragraphDiv = document.querySelector('.paragraphWrapper');
+
+//function to create element
+let postParagraph = () => {
+
+    // Element is called newParagraph and created through createElement
+    let newParagraph = document.createElement('p');
+
+    // Paragraph text content added 
+    newParagraph.textContent = "This paragraph has been created";
+
+    // newPara appended to parent (paragraphDiv)
+    paragraphDiv.appendChild(newParagraph);
 }
 
-let appendChild = () => {
-    
-}
+// Invokes the postParagraph function
+createElementBtn.addEventListener('click', postParagraph);
+
 // * Remove Child
+// Removes a selected child from a parent div
+
+// Assigning button to variable removeElement
+let removeElementBtn = document.querySelector('.removeChildBtn');
+console.log(removeElementBtn);
+
+// Function to remove a paragraph
+let removeParagraph = () => {
+
+    // Using queryselector to find the correct paragraph
+    let paragraphToRemove = document.querySelector('div > *');
+
+    // Console log to print out the paragraph
+    console.log(paragraphToRemove);
+
+    // Removing the selected paragraph from the parent div
+    paragraphDiv.removeChild(paragraphToRemove);
+    console.log("Paragraph removed!");
+}
+
+// Adding event listener to button
+removeElementBtn.addEventListener('click', removeParagraph);
 
 // * Replace Child
+// Replacing an old child element with a new child element
+// Find the old child element to replace
+// Create a new element
+// Swap the elements around
+
+let replaceChildBtn = document.querySelector('.replaceChildBtn');
+
+let replaceParagraph = () => {
+
+    console.log("Update ran");
+
+    // Using queryselector to find the correct paragraph
+    let paragraphToUpdate = document.querySelector('div > p');
+
+    // Element is called updatedHeader and created through createElement
+    let updatedHeader = document.createElement('h2');
+
+    // Adding text content to element
+    updatedHeader.textContent = "This is an updated element!";
+
+    // Replace the children
+    paragraphDiv.replaceChild(updatedHeader, paragraphToUpdate);
+}
+
+// Invokes function to replace a child
+replaceChildBtn.addEventListener('click', replaceParagraph);
+
 
 // * Set Attribute
+// Allows you to add a new or update the value of an existing attribute with a HTML element
+
+// Find the element we want to manipulate 
+
+
+let updateAttribute = () => {
+    headerClass.setAttribute('contenteditable','');
+    headerClass.setAttribute('style', 'color: purple');
+}
+
+let setAttributeBtn = document.querySelector('.setAttribute');
+setAttributeBtn.addEventListener('click', updateAttribute);
 
 // * Get Attribute
+// gets an attribute
+
+h2AttributeStr = headerClass.getAttribute('style');
+
+console.log(headerClass.getAttribute('style'));
+console.log(`String taken from attribute: ${h2AttributeStr}`);
+
 
 // * Remove Attribute
+// Removes an attribute
 
+headerClass.removeAttribute('style');
 
+// TODO - Exercise!
 
+// Buttons added to HTML that do the following functions: 
+// * Create a List item within a type of list (Stretch goal - Using input form to type out new content)
+// * Remove a list item within the list
+// * Update the content of the list (Stretch goal - Using input form to type out new content)
+// * Change attribute of all lists (CSS/Contenteditable/up to you)
+// * Remove all attributes from list items
+// * Stretch goal - Change attributes / Update specific item (NOT JUST TOP ONE) 
 
 
 
